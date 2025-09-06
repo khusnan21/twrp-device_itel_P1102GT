df3471ef50ab54445f2b522efe6c57a99a4aa49c
khusnan21 <khusnan21@gmail.com>
2025-09-07 01:55:24 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,8 +6,3 @@
 
 # Inherit from mt6789-common
 $(call inherit-product, device/transsion/mt6789-common/common.mk)
-
-PRODUCT_EXTRA_RECOVERY_KEYS += \
-    $(DEVICE_PATH)/security/releasekey
-
-TARGET_OTA_CERTIFICATE := device/itel/P1102GT/security/releasekey.x509.pem
